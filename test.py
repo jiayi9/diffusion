a8db236072fedb298d3deb599cc5ef25f390d264
Jiayi Lu <jiayi.lu1@astrazeneca.com>
2023-04-11 17:32:20 +0800

    xx

---
+++ test.py
@@ -0,0 +1,117 @@
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+
+def show_images(data, num_samples=20, cols=4):
+    """ Plots some samples from the dataset """
+    plt.figure(figsize=(15,15))
+    for i, img in enumerate(data):
+        if i == num_samples:
+            break
+        plt.subplot(int(num_samples/cols + 1), cols, i + 1)
+        plt.imshow(img[0])
+
+# data = torchvision.datasets.StanfordCars(root="C:/Temp/cars", download=True)
+# show_images(data)
+
+
+
+def linear_beta_schedule(timesteps, start=0.0001, end=0.02):
+    return torch.linspace(start, end, timesteps)
+
+
+def get_index_from_list(vals, t, x_shape):
+    """
+    Returns a specific index t of a passed list of values vals
+    while considering the batch dimension.
+    """
+    batch_size = t.shape[0]
+    out = vals.gather(-1, t.cpu())
+    return out.reshape(batch_size, *((1,) * (len(x_shape) - 1))).to(t.device)
+
+
+def forward_diffusion_sample(x_0, t, device="cpu"):
+    """
+    Takes an image and a timestep as input and
+    returns the noisy version of it
+    """
+    noise = torch.randn_like(x_0)
+    sqrt_alphas_cumprod_t = get_index_from_list(sqrt_alphas_cumprod, t, x_0.shape)
+    sqrt_one_minus_alphas_cumprod_t = get_index_from_list(
+        sqrt_one_minus_alphas_cumprod, t, x_0.shape
+    )
+    # mean + variance
+    return sqrt_alphas_cumprod_t.to(device) * x_0.to(device) \
+    + sqrt_one_minus_alphas_cumprod_t.to(device) * noise.to(device), noise.to(device)
+
+
+# Define beta schedule
+T = 300
+betas = linear_beta_schedule(timesteps=T)
+
+# Pre-calculate different terms for closed form
+alphas = 1. - betas
+alphas_cumprod = torch.cumprod(alphas, axis=0)
+
+len(alphas_cumprod)
+
+alphas_cumprod_prev = F.pad(alphas_cumprod[:-1], (1, 0), value=1.0)
+
+len(alphas_cumprod_prev)
+
+alphas_cumprod[0:10]
+alphas_cumprod_prev[0:10]
+
+alphas_cumprod[-10:]
+alphas_cumprod_prev[-10:]
+
+sqrt_recip_alphas = torch.sqrt(1.0 / alphas)
+
+sqrt_alphas_cumprod = torch.sqrt(alphas_cumprod)
+sqrt_one_minus_alphas_cumprod = torch.sqrt(1. - alphas_cumprod)
+posterior_variance = betas * (1. - alphas_cumprod_prev) / (1. - alphas_cumprod)
+
+
+
+
+from torchvision import transforms
+from torch.utils.data import DataLoader
+import numpy as np
+
+IMG_SIZE = 32
+BATCH_SIZE = 16
+
+
+
+def load_transformed_dataset():
+    data_transforms = [
+        transforms.Resize((IMG_SIZE, IMG_SIZE)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(), # Scales data into [0,1]
+        transforms.Lambda(lambda t: (t * 2) - 1) # Scale between [-1, 1]
+    ]
+    data_transform = transforms.Compose(data_transforms)
+
+    train = torchvision.datasets.StanfordCars(root=".", download=True,
+                                         transform=data_transform)
+
+    test = torchvision.datasets.StanfordCars(root=".", download=True,
+                                         transform=data_transform, split='test')
+    return torch.utils.data.ConcatDataset([train, test])
+def show_tensor_image(image):
+    reverse_transforms = transforms.Compose([
+        transforms.Lambda(lambda t: (t + 1) / 2),
+        transforms.Lambda(lambda t: t.permute(1, 2, 0)), # CHW to HWC
+        transforms.Lambda(lambda t: t * 255.),
+        transforms.Lambda(lambda t: t.numpy().astype(np.uint8)),
+        transforms.ToPILImage(),
+    ])
+
+    # Take first image of batch
+    if len(image.shape) == 4:
+        image = image[0, :, :, :]
+    plt.imshow(reverse_transforms(image))
+
+data = load_transformed_dataset()
+dataloader = DataLoader(data, batch_size=BATCH_SIZE, shuffle=True, drop_last=True)
